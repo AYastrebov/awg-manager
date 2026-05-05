3e6e2e8badc2e991702c106388abe85d570fb9cd
hoaxisr <avs@rusimp.com>
2026-05-05 15:26:13 +0000

    feat(icons): add qrcode, chevron-right, player-stop

--- frontend/src/lib/components/ui/tabler-icons.ts
+++ frontend/src/lib/components/ui/tabler-icons.ts
@@ -117,6 +117,23 @@ export const TABLER_ICONS = {
   <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6" />
   <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" />
   <path d="M8 11v-4a4 4 0 1 1 8 0v4" />`,
+  Qrcode: `<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  <path d="M4 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1l0 -4" />
+  <path d="M7 17l0 .01" />
+  <path d="M14 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1l0 -4" />
+  <path d="M7 7l0 .01" />
+  <path d="M4 15a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1l0 -4" />
+  <path d="M17 7l0 .01" />
+  <path d="M14 14l3 0" />
+  <path d="M20 14l0 .01" />
+  <path d="M14 14l0 3" />
+  <path d="M14 20l3 0" />
+  <path d="M17 17l3 0" />
+  <path d="M20 17l0 3" />`,
+  ChevronRight: `<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  <path d="M9 6l6 6l-6 6" />`,
+  PlayerStop: `<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  <path d="M5 7a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2l0 -10" />`,
 } as const;
 
 export type TablerIconName = keyof typeof TABLER_ICONS;
